c86671a41f1f723b42eca034fd04b180e8621ba6
Alex <alexvillafana99@gmail.com>
2025-04-02 12:35:28 -0700

    Added signup functionality. Sent to mongo db

---
+++ backend/routes/api/signup.js
@@ -0,0 +1,49 @@
+const express = require('express'); // Import express to create a router
+const router = express.Router(); // Create a new router object
+const bcryptjs = require('bcryptjs'); // Import bcryptjs for password hashing and comparison
+const User = require('../../models/user'); // Import the User model to interact with the database
+
+// POST /api/signup
+router.post('/', async (req, res) => {
+    const {username, password} = req.body; // Destructure the request body to get username and password
+
+    try {
+        // Check if username and password are provided
+        if (!username) {
+            console.log('Username is required');
+            return res.status(400).json({message: 'Username is required'});
+        }
+
+        if (!password) {
+            console.log('Password is required');
+            return res.status(400).json({message: 'Password is required'});
+        }
+
+        // Hash the password using bcryptjs before storing it
+        const saltRounds = 10; // Salt rounds for hashing
+
+        // Store the hashed password 
+        const hashedPassword = await bcryptjs.hash(password, saltRounds); 
+
+        // Create a new user object
+        const newUser = new User({
+            username, password: hashedPassword
+        })
+
+        console.log(`New user created: ${username}`);
+        console.log(`Hashed password: ${hashedPassword}`);
+
+        // Save the new user to the database
+        await newUser.save();
+
+        res.status(201).json({message: 'User created successfully'}); // Return success message
+
+
+
+    } catch(error){
+        console.error(`Error: ${error.message}`);
+        return res.status(500).json({message: 'Server error'});
+    }
+})
+
+module.exports = router; // Export the router so it can be used in other files
