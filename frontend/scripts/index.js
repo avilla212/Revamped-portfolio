9c16338c3ea3c760f4e5d185fb7bee3a374af88e
Alex <alexvillafana99@gmail.com>
2025-04-09 12:59:24 -0700

    Fixed logic error where homepage was getting served even if user wasnt logged in

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -8,3 +8,41 @@
 // 2. Check mongodb for user
 // 3. Verification of password
 // 4. Return success or failure
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("login-form");
+
+    form?.addEventListener("submit", async (event) => {
+        event.preventDefault(); // Prevent the default form submission
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        try {
+            const res = await fetch("/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                }),
+            })
+
+            const data = await res.json(); // Parse the JSON response
+
+            if (res.ok) {
+                alert('Login successful!'); // Show success message
+                // If the response is ok, redirect to the dashboard
+                window.location.href = "/homepage.html";
+            } else {
+                alert(data.message); // Show error message
+            }
+        } catch (error){
+            console.error(`Error: ${error.message}`); // Log the error message
+            alert('An error occurred while logging in. Please try again.'); // Show error message
+        }
+
+    });
+})
---
+++ frontend/scripts/sign-in.js
@@ -0,0 +1,2 @@
+// Sign in 
+// This script handles the sign-in process for the application.
